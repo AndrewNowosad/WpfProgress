a6a03720349da93e09450e9fc7988192d91c31f6
Андрей <nowosad@inbox.ru>
2017-08-05 07:41:48 +0300

    Добавлен конвертер Значение -> Координаты точки на дуге

---
+++ WpfProgress/ValueToAuxiliaryPointConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfProgress
+{
+    class ValueToAuxiliaryPointConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Contains(DependencyProperty.UnsetValue))
+                return DependencyProperty.UnsetValue;
+            var v = (double)values[0];
+            var min = (double)values[1];
+            var max = (double)values[2];
+            var ratio = (v - min) / (max - min);
+            var angle = Math.PI * ratio;
+            var x = 1 - Math.Cos(angle);
+            var y = 1 - Math.Sin(angle);
+            return new Point(x, y);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
